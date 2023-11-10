77485ce77c2d3092c1dbb65295e77aa13f4dfa60
bakidkbyk <baki.ordulu@gmail.com>
2023-11-10 11:05:26 +0300

    home header cell

--- UIComponents/UIComponents/HomeCell/HomeMovieHeaderCell/HomeMovieHeaderCell.swift
+++ UIComponents/UIComponents/HomeCell/HomeMovieHeaderCell/HomeMovieHeaderCell.swift
@@ -9,25 +9,59 @@ import UIKit
 
 public class HomeMovieHeaderCell: UICollectionViewCell, ReusableView {
     
+    private let imageView = UIImageViewBuilder()
+        .contentMode(.scaleToFill)
+        .build()
+    
+    private let titleLabel = UILabelBuilder()
+        .textColor(.white)
+        .font(.font(.nunitoBold, size: .xLarge))
+        .build()
+    
+    private let descriptionLabel = UILabelBuilder()
+        .textColor(.black)
+        .font(.font(.nunitoBold, size: .medium))
+        .build()
+    
     weak var viewModel: HomeMovieHeaderCellProtocol?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configureContents()
+        addSubviews()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        configureContents()
+        addSubviews()
     }
+}
+
+// MARK: - UILauyout
+extension HomeMovieHeaderCell {
     
-    private func configureContents() {
+    private func addSubviews() {
+        contentView.addSubview(imageView)
+        imageView.edgesToSuperview()
+        
+        contentView.addSubview(titleLabel)
+        titleLabel.edgesToSuperview(excluding: [.bottom, .trailing], insets: .init(top: 200, left: 20, bottom: 0, right: 0))
+        
+        contentView.addSubview(descriptionLabel)
+        descriptionLabel.topToBottom(of: titleLabel).constant = 20
+        descriptionLabel.leadingToSuperview().constant = 20
+        descriptionLabel.trailingToSuperview().constant = -20
         
     }
+}
+
+// MARK: - Set View Model
+extension HomeMovieHeaderCell {
     
     public func set(viewModel: HomeMovieHeaderCellProtocol) {
         self.viewModel = viewModel
-        
+        imageView.setImage(viewModel.backdropPath ?? "")
+        titleLabel.text = viewModel.title
+        descriptionLabel.text = viewModel.overview
     }
     
 }
--- UIComponents/UIComponents/HomeCell/HomeMovieHeaderCell/HomeMovieHeaderCellModel.swift
+++ UIComponents/UIComponents/HomeCell/HomeMovieHeaderCell/HomeMovieHeaderCellModel.swift
@@ -8,7 +8,10 @@
 import Foundation
 
 public protocol HomeMovieHeaderCellDataSource: AnyObject {
-    
+    var backdropPath: String? { get set }
+    var id: Int { get set }
+    var title: String? { get set }
+    var overview: String? { get set }
 }
 
 public protocol HomeMovieHeaderCellEventSource: AnyObject {
@@ -21,4 +24,15 @@ public protocol HomeMovieHeaderCellProtocol: HomeMovieHeaderCellDataSource, Home
 
 public final class HomeMovieHeaderCellModel: HomeMovieHeaderCellProtocol {
     
+    public var backdropPath: String?
+    public var id: Int
+    public var title: String?
+    public var overview: String?
+    
+    public init(backdropPath: String?, id: Int, title: String?, overview: String?) {
+        self.backdropPath = backdropPath
+        self.id = id
+        self.title = title
+        self.overview = overview
+    }
 }
