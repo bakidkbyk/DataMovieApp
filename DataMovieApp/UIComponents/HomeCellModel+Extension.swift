8f5b55d2b7f15244decd6084b8639fe1673a99c2
bakidkbyk <baki.ordulu@gmail.com>
2023-11-15 07:58:54 +0300

    aa home extension

---
+++ DataMovieApp/UIComponents/HomeCellModel+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  HomeCellModel+Extension.swift
+//  DataMovieApp
+//
+//  Created by Baki Dikbıyık on 13.11.2023.
+//
+
+import MobilliumDateFormatter
+
+extension HomeCellModel {
+    
+    convenience init(dataMovieInfo: DataMovieInfo) {
+    
+        let date = Date.from(dataMovieInfo.date ?? "", format: .custom(rawValue: "yyyy-MM-dd"))
+        let dateToString = date?.to(.custom(rawValue: "dd-MM-yyyy"))
+        
+        self.init(movieId: dataMovieInfo.movieId,
+                  backdropPath: dataMovieInfo.backdropPath,
+                  title: dataMovieInfo.title,
+                  overview: dataMovieInfo.overView,
+                  date: dateToString)
+    }
+}
