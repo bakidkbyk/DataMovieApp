7657437d26a80da86aa75ec2b4584732d3818631
bakidkbyk <baki.ordulu@gmail.com>
2023-12-03 07:11:30 +0300

    home

--- DataMovieApp/Scenes/Home/HomeViewModel.swift
+++ DataMovieApp/Scenes/Home/HomeViewModel.swift
@@ -5,7 +5,10 @@
 //  Created by Baki Dikbıyık on 24.10.2023.
 //
 
-protocol HomeViewDataSource {}
+protocol HomeViewDataSource {
+    func numberOfItemsAt() -> Int
+    func cellItemAt(_ indexPath: IndexPath) -> HomeCellProtocol
+}
 
 protocol HomeViewEventSource {}
 
@@ -13,4 +16,127 @@ protocol HomeViewProtocol: HomeViewDataSource, HomeViewEventSource {}
 
 final class HomeViewModel: BaseViewModel<HomeRouter>, HomeViewProtocol {
     
+    var page = 1
+    var isRequestEnabled = false
+    var isPagingEnabled = false
+    
+    private var isGetNowPlayingMovieSuccess = false
+    private var isGetUpcomingMovieSuccess = false
+    
+    private let dispatchGroup = DispatchGroup()
+    
+    var reloadData: VoidClosure?
+    var endRefreshing: VoidClosure?
+    var getDataDidSuccess: VoidClosure?
+    
+    var homecellItems: [HomeCellProtocol] = []
+    var homeHeaderMovieCellItems: [HomeMovieHeaderCellProtocol] = []
+    
+    func numberOfItemsAt() -> Int {
+        return homecellItems.count
+    }
+    
+    func cellItemAt(_ indexPath: IndexPath) -> HomeCellProtocol {
+        return homecellItems[indexPath.row]
+    }
+    
+    override func tryAgainButtonTapped() {
+        self.hideTryAgainButton?()
+        getData(showLoading: true)
+    }
+    
+    func resetData() {
+        homecellItems.removeAll()
+        homeHeaderMovieCellItems.removeAll()
+        page = 1
+        isGetNowPlayingMovieSuccess = false
+        isGetUpcomingMovieSuccess = false
+        getData(showLoading: false)
+    }
+}
+
+// MARK: - Actions
+extension HomeViewModel {
+    
+    func didSelectHeaderMovie(indexPath: IndexPath) {
+        // TODO: - Detail Sayfasına yönlendirme sağlanacak
+    }
+    
+    func didSelectMovieDetail(indexPath: IndexPath) {
+        // TODO: - Detail Sayfasına yönlendirme sağlanacak
+    }
+}
+
+// MARK: - Network
+extension HomeViewModel {
+    
+    func nowPlayingDataMovieRequest() {
+        let request = NowPlayingDataMovieRequest()
+        dispatchGroup.enter()
+        dataProvider.request(for: request) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let response):
+                let homeHeaderCellItems = response.results.map({ HomeMovieHeaderCellModel(dataMovieInfo: $0) })
+                self.homeHeaderMovieCellItems.append(contentsOf: homeHeaderCellItems)
+                self.reloadData?()
+                self.isGetNowPlayingMovieSuccess = true
+            case .failure(let error):
+                self.showSuccessToast?(error.localizedDescription)
+            }
+            self.dispatchGroup.leave()
+        }
+    }
+    
+    func getUpcomingDataMovieRequest() {
+        let request = UpcomingDataMovieRequest(page: page)
+        dispatchGroup.enter()
+        dataProvider.request(for: request) { [weak self] result in
+            guard let self = self else { return }
+            self.isRequestEnabled = true
+            switch result {
+            case .success(let response):
+                let cellItems = response.results.map({ HomeCellModel(dataMovieInfo: $0) })
+                self.homecellItems.append(contentsOf: cellItems)
+                self.reloadData?()
+                self.page += 1
+                self.isGetUpcomingMovieSuccess = true
+                self.isPagingEnabled = response.totalPage > response.page
+            case .failure(let error):
+                self.showWarningToast?(error.localizedDescription)
+            }
+            self.isRequestEnabled = false
+            self.dispatchGroup.leave()
+        }
+    }
+}
+
+// MARK: - Dispatch Group
+extension HomeViewModel {
+    
+    func getData(showLoading: Bool) {
+        
+        if showLoading && (!isGetUpcomingMovieSuccess || !isGetNowPlayingMovieSuccess ) {
+            self.endRefreshing?()
+            showActivityIndicatorView?()
+        }
+        
+        if !isGetNowPlayingMovieSuccess {
+            nowPlayingDataMovieRequest()
+        }
+        
+        if !isGetUpcomingMovieSuccess {
+            getUpcomingDataMovieRequest()
+        }
+        
+        dispatchGroup.notify(queue: DispatchQueue.main) { [weak self] in
+            guard let self = self else { return }
+            self.hideActivityIndicatorView?()
+            if self.isGetUpcomingMovieSuccess && self.isGetNowPlayingMovieSuccess {
+                self.getDataDidSuccess?()
+            } else {
+                self.showTryAgainButton?()
+            }
+        }
+    }
 }
